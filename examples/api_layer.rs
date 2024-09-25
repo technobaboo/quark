9a99b751be790866d4e16ffeefded4e9762f3553
Nova <technobaboo@gmail.com>
2024-09-24 22:43:01 -0400

    feat: code should worky? awa?

--- examples/api_layer.rs
+++ examples/api_layer.rs
@@ -1,27 +1,29 @@
 use openxr::{
     sys::{Instance, InstanceCreateInfo, Result as XrErr},
-    Entry,
+    Entry, InstanceExtensions,
 };
 use quark::{api_layer, prelude::*, APILayerInstanceData};
 
 #[handle(openxr::sys::Instance)]
 pub struct InstanceData {
-    entry: Entry,
+    instance: openxr::Instance,
 }
 impl APILayerInstanceData for InstanceData {
     fn create(
         entry: Entry,
         instance_info: &InstanceCreateInfo,
-        _raw_instance: Instance,
+        instance: Instance,
     ) -> Result<Self, XrErr> {
         let app_name = unsafe {
             str_from_const_char(instance_info.application_info.application_name.as_ptr())?
         };
         println!("got your new instance chief, app's named {app_name}");
-        Ok(InstanceData { entry })
+        let instance =
+            unsafe { openxr::Instance::from_raw(entry, instance, InstanceExtensions::default())? };
+        Ok(InstanceData { instance })
     }
     fn entry(&self) -> &Entry {
-        &self.entry
+        self.instance.entry()
     }
 }
 
@@ -35,6 +37,9 @@ api_layer! {
 pub fn main() {}
 
 #[openxr(xrCreateActionSet)]
-pub fn create_action_set() -> XrResult {
-    todo!()
+pub fn create_action_set(instance: Instance) -> XrResult {
+    let data: &InstanceData = &*InstanceData::borrow_raw(instance)?;
+    data.instance.create_action_set("uwu", "owo", 30)?;
+
+    Ok(())
 }
